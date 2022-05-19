e382efd9ca97ec6bce55022deacc3c843d3b67bc
ludogwada <83616314+ludogwada@users.noreply.github.com>
2022-05-19 07:53:36 -0400

    start

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { 
+  BrowserRouter as Router,
+  Route, 
+  Routes,
+  } from 'react-router-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Header from './components/Header/header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<Router>
+			<Header />
+			<Routes>
+				<Route />
+			</Routes>
+		</Router>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
