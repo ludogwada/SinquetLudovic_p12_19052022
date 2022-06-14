c851d7c8d894bdc99f8d80fbbaf47f6dc7b0fa14
ludogwada <83616314+ludogwada@users.noreply.github.com>
2022-06-14 10:15:37 -0400

    Modif callDataMocked

--- src/utils/Api/callDataMocked.jsx
+++ src/utils/Api/callDataMocked.jsx
@@ -1,51 +1,41 @@
-import { useState } from "react";
 import { USER_ACTIVITY, USER_MAIN_DATA, USER_AVERAGE_SESSIONS, USER_PERFORMANCE } from "../../datas/data";
 
-function ApiUser(id) {
-    
-    const [userInfo] = useState(USER_MAIN_DATA)
-    
-    // eslint-disable-next-line
-    let user = userInfo.find( user => user.id == id)
-    console.log(user);
-    return user;
 
+async function ApiUser(id) {
+    try {
+        const res = USER_MAIN_DATA.find((el) => el.userId == id);
+        return {data:res};
+    } catch (e) {
+        console.log(e);
+    }
 }
 
-
-function ApiActivity(id) {
-
-    const [userActivity] = useState(USER_ACTIVITY)
-    
-    // eslint-disable-next-line
-    let dailyActivity = userActivity.find( user => user.userId == id)
-
-    return dailyActivity
-
+async function ApiActivity(id) {
+    try {
+        const res = USER_ACTIVITY.find((el) => el.userId == id);
+        return {data:res};
+    } catch (e) {
+        console.log(e);
+    }
 }
 
-function ApiAverage(id) {
-
-    const [userAverageSession] = useState(USER_AVERAGE_SESSIONS)
-    
-    // eslint-disable-next-line
-    let averageSession = userAverageSession.find( user => user.userId == id)
-
-    return averageSession
+async function ApiAverage(id) {
+    try {
+		const res = USER_AVERAGE_SESSIONS.find((el) => el.userId == id);
+        return {data:res};
+    } catch (e) {
+        console.log(e);
+    }
 }
 
-function ApiPerformance(id) {
-
-    const [userPerformance] = useState(USER_PERFORMANCE)
-    
-    // eslint-disable-next-line
-    let performance = userPerformance.find( user => user.userId == id)
-
-    return {performance}
-
+async function ApiPerformance(id) {
+    try {
+		const res = USER_PERFORMANCE.find((el) => el.userId == id);
+        return {data:res};
+    } catch (e) {
+        console.log(e);
+    }
 }
 
-
-export {ApiUser, ApiActivity, ApiAverage, ApiPerformance}
-
+export { ApiUser, ApiActivity, ApiAverage, ApiPerformance}
 
